c48a6c1c0a1004ade13bcc296d85880b3946d9ef
Mark Bryginskii <m.bryginskii@gmail.com>
2020-12-04 03:44:40 +0700

    fix: fix backend part

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,13 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 app.use('/', usersRouters);
 
 app.use(auth);
