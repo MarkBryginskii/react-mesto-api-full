3021e578050196af7f0658ca533058cb3e9bebbd
Mark Bryginskii <m.bryginskii@gmail.com>
2020-12-05 23:12:59 +0700

    fix: fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,7 +9,9 @@ const NotFoundError = require('../errors/NotFoundError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const createUser = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  const { email } = req.body;
+
+  User.findOne(email)
     .then((user) => {
       if (user) {
         throw new RequestError('Такой пользователь уже существует в системе');
